3556bd4cceca70c8c6f2d1fd948ee25aaf120c0a
Margarida Silva <ms@platforme.com>
2022-03-13 14:01:23 +0000

    style: pretty

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
-import React, { useState } from 'react';
-import { Button, Image, SafeAreaView, StyleSheet } from 'react-native';
-import { Draw } from 'react-native-draw-image';
+import React, { useState } from "react";
+import { Button, Image, SafeAreaView, StyleSheet } from "react-native";
+import { Draw } from "react-native-draw-image";
 
 const App = () => {
     const [image, setImage] = useState(null);
     const [color, setColor] = useState("#ff0000");
 
     const randomColor = () => {
-        let color = '#';
+        let rColor = "#";
         for (let i = 0; i < 6; i++) {
             const random = Math.random();
+
+            // eslint-disable-next-line no-bitwise
             const bit = (random * 16) | 0;
-            color += (bit).toString(16);
-        };
-        return color;
+            rColor += bit.toString(16);
+        }
+        return rColor;
     };
 
     const onPress = async () => {
-        const image = await this.draw?.export();
-        setImage(image)
-    }
+        const newImage = await this.draw?.export();
+        setImage(newImage);
+    };
 
     const onUndoPress = () => {
         this.draw.undo();
-    }
+    };
 
     const onColorPress = () => {
-        setColor(randomColor())
-    }
+        setColor(randomColor());
+    };
 
     return (
         <SafeAreaView style={styles.app}>
-            {image ?
+            {image ? (
                 <Image style={{ flex: 1 }} source={{ uri: `data:image/png;base64,${image}` }} />
-                : <Draw
+            ) : (
+                <Draw
                     ref={el => (this.draw = el)}
                     strokeColor={color}
-                    uri={"https://yt3.ggpht.com/a/AATXAJzY05FWFkxKyQxF28iHkGxHTbElWqFAsnnYyA=s900-c-k-c0xffffffff-no-rj-mo"}
+                    uri={
+                        "https://yt3.ggpht.com/a/AATXAJzY05FWFkxKyQxF28iHkGxHTbElWqFAsnnYyA=s900-c-k-c0xffffffff-no-rj-mo"
+                    }
                 />
-            }
+            )}
             <Button title="Export" onPress={onPress} />
             <Button title="Undo" onPress={onUndoPress} />
             <Button title="Switch color" onPress={onColorPress} />
